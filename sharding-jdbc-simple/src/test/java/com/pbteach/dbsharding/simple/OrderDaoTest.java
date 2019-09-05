679a6b88197d67287dfddbb548b8f0abbb2ebca9
pbteach <pbteach@126.com>
2019-09-06 07:32:49 +0800

    提交代码

---
+++ sharding-jdbc-simple/src/test/java/com/pbteach/dbsharding/simple/OrderDaoTest.java
@@ -0,0 +1,43 @@
+package com.pbteach.dbsharding.simple;
+
+import com.pbteach.dbsharding.simple.dao.OrderDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShardingJdbcSimpleBootstrap.class)
+public class OrderDaoTest {
+
+    @Autowired
+    OrderDao orderDao;
+
+    @Test
+    public void testInsertOrder(){
+        for(int i=1;i<11;i++){
+            orderDao.insertOrder(new BigDecimal(i),3L,"SUCCESS");
+        }
+        for(int i=1;i<11;i++){
+            orderDao.insertOrder(new BigDecimal(i),4L,"SUCCESS");
+        }
+    }
+    @Test
+    public void testSelectOrderbyIds(){
+        List<Long> ids = new ArrayList<>();
+        ids.add(373771636085620736L);
+        ids.add(373771635804602369L);
+        List<Map> maps = orderDao.selectOrderbyIds(ids);
+        System.out.println(maps);
+    }
+}
---
+++ shopping/src/main/java/com/pbteach/shopping/ShoppingBootstrap.java
@@ -0,0 +1,13 @@
+package com.pbteach.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingBootstrap.class, args);
+    }
+
+}
